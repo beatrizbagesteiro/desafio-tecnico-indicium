142961c67dea408c0f0b93d208168a3b78bef015
Beatriz <beatriz_bagesteiro@hotmail.com>
2026-08-10 14:19:24 -0300

    feat: configuraçoes iniciais

---
+++ src/sql/eda_orders.sql
@@ -0,0 +1,60 @@
+SELECT * 
+FROM 'data/raw/orders.csv' 
+LIMIT 100;
+
+-- Total de linhas
+SELECT COUNT(*) AS total_linhas
+FROM 'data/raw/orders.csv';
+
+-- Total de colunas
+SELECT COUNT(*) AS total_colunas
+FROM (DESCRIBE 'data/raw/orders.csv');
+
+-- Informações sobre as colunas
+DESCRIBE 'data/raw/orders.csv';
+
+-- Intervalo de datas (2020-2026)
+SELECT 
+    MIN(created_at) AS menor_data,
+    MAX(created_at) AS maior_data
+FROM 'data/raw/orders.csv';
+
+-- Análise da coluna 'total'
+SELECT 
+    MIN(total) AS valor_minimo,
+    MAX(total) AS valor_maximo,
+    AVG(total) AS valor_medio
+FROM 'data/raw/orders.csv';
+
+-- Não existe pedidos duplicados nessa tabela
+SELECT COUNT(DISTINCT order_number) 
+FROM 'data/raw/orders.csv';
+
+-- Valores nulos
+-- Única coluna que possui valores nulos é sales_person_id
+SELECT 
+    COUNTIF(id IS NULL) as id_nulls,
+    COUNTIF(order_number IS NULL) as order_number_nulls,
+    COUNTIF(channel IS NULL) as channel_nulls,
+    COUNTIF(customer_id IS NULL) as customer_id_nulls,
+    COUNTIF(salesperson_id IS NULL) as salesperson_id_nulls,
+    COUNTIF(location_id IS NULL) as location_id_nulls,
+    COUNTIF(status IS NULL) as status_nulls,
+    COUNTIF(subtotal IS NULL) as subtotal_nulls,
+    COUNTIF(discount_amount IS NULL) as discount_amount_nulls,
+    COUNTIF(total IS NULL) as total_nulls,
+    COUNTIF(placed_at IS NULL) as placed_at_nulls,
+    COUNTIF(created_at IS NULL) as created_at_nulls,
+    COUNTIF(updated_at IS NULL) as updated_at_nulls
+FROM 'data/raw/orders.csv';
+
+-- Não tem inconsistencia
+SELECT status 
+FROM 'data/raw/orders.csv'
+GROUP BY status;
+
+-- Não tem inconsistencia
+SELECT channel 
+FROM 'data/raw/orders.csv'
+GROUP BY channel;
+
