adcaa299397e3f4bc3c519f09a31c4f9f74f2550
Beatriz <beatriz_bagesteiro@hotmail.com>
2026-08-11 13:33:19 -0300

    feat: ead básico completo

--- src/sql/eda_orders.sql
+++ src/sql/eda_orders.sql
@@ -75,6 +75,18 @@ SELECT order_number, placed_at, created_at, updated_at
 FROM 'data/raw/orders.csv'
 WHERE NOW() < placed_at;
 
+-- Análise location_id 
+-- Tudo de acordo
+SELECT 
+    l.is_active, 
+    l.created_at, 
+    l.updated_at, 
+    o.location_id, 
+    o.placed_at, 
+    o.updated_at 
+FROM 'data/raw/orders.csv' AS o 
+JOIN 'data/raw/locations.csv' AS l ON o.location_id = l.id
+WHERE l.is_active = false AND o.placed_at > l.updated_at; 
 
 -- Análise da coluna "total"
 SELECT 
@@ -107,5 +119,45 @@ SELECT
     discount_amount, 
     total
 FROM 'data/raw/orders.csv'
-WHERE total < 0
+WHERE total < 0;
 
+-- Verificar se os cálculos estão aplicados corretamente
+WITH aux AS (
+    SELECT 
+        o.order_id,
+        SUM(pv.sale_price * o.quantity) AS total_bruto
+    FROM 'data/raw/order_items.csv' as o
+    JOIN 'data/raw/product_variants.csv' as pv ON pv.id = o.product_variant_id
+    GROUP BY o.order_id
+)
+SELECT 
+    ord.id, 
+    aux.total_bruto,
+    ord.subtotal
+FROM 'data/raw/orders.csv' AS ord
+JOIN aux ON ord.id = aux.order_id 
+WHERE ROUND(aux.total_bruto, 2) <> ROUND(ord.subtotal, 2);
+
+-- Retorna valores que estão acima ou abaixo dos limites calculados
+WITH quartis AS (
+    SELECT 
+        QUANTILE_CONT(total, 0.25) AS q1,
+        QUANTILE_CONT(total, 0.75) AS q3 
+    FROM 'data/raw/orders.csv'
+    WHERE status IN ('paid', 'confirmed')
+),
+limites AS (
+    SELECT 
+        q1, 
+        q3, 
+        (q3 - q1) AS iqr,
+        (q1 - 1.5 * (q3 - q1)) AS limite_inferior,
+        (q3 + 1.5 * (q3 - q1)) AS limite_superior
+    FROM quartis
+)
+SELECT 
+    o.id, 
+    o.total
+FROM 'data/raw/orders.csv' AS o
+CROSS JOIN limites AS l
+WHERE o.total < l.limite_inferior OR o.total > l.limite_superior;
